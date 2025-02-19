10f0bdb565ecc18adb462d9071534eb4426dc4da
raselm282 <raselm282@gmail.com>
2025-02-19 18:44:37 +0600

    add vercel url

--- src/Components/Home/AboutMe.jsx
+++ src/Components/Home/AboutMe.jsx
@@ -5,7 +5,10 @@ const AboutMe = () => {
   return (
     <div>
       <section className="bg-white dark:bg-gray-900">
-      <SectionTitle heading="About Me" subHeading="I am a passionate and skilled developer with expertise in building dynamic, responsive web applications. With a strong foundation in both front-end and back-end technologies, I strive to create efficient and user-friendly solutions that meet both client and user needs. 📊"></SectionTitle>
+        <SectionTitle
+          heading="About Me"
+          subHeading="I am a passionate and skilled developer with expertise in building dynamic, responsive web applications. With a strong foundation in both front-end and back-end technologies, I strive to create efficient and user-friendly solutions that meet both client and user needs. 📊"
+        ></SectionTitle>
         {/* <h2 className="text-3xl font-bold text-center text-gray-900 dark:text-white mb-4">
           About Me
         </h2> */}
@@ -22,6 +25,18 @@ const AboutMe = () => {
           thought, and promote understanding. I aim to be a trusted source of
           information, one that educates and empowers my audience to stay
           informed and make meaningful decisions.
+        </p>
+        <p>
+          I am a passionate Web Developer with a strong foundation in front-end
+          and back-end technologies. I specialize in building dynamic,
+          responsive, and user-friendly web applications using React, Tailwind
+          CSS, Node.js, Express, and MongoDB. With experience in full-stack
+          development, I enjoy crafting seamless digital experiences that
+          combine creativity with functionality. I am always eager to learn new
+          technologies and stay updated with the latest trends in web
+          development. Whether it's designing engaging UI/UX or developing
+          robust backend systems, I strive to create high-quality, efficient,
+          and scalable solutions. Let's build something amazing together! 🚀
         </p>
       </section>
     </div>
